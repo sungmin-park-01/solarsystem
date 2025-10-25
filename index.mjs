efbe9fdecc37566cf753c71712d42b24c6e1ab34
sungmin-park-01 <llime4395@gmail.com>
2025-10-24 21:57:00 -0700

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -1,38 +1,54 @@
 import express from 'express';
 import fetch from 'node-fetch';
-const solarSystem = (await import('npm-solarsystem')).default;
-
+const solarSystem = (await import("npm-solarsystem")).default;
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', async (req, res) => {
-   let url = "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&per_page=50&orientation=horizontal&q=solar system";
-   let response = await fetch(url)
-   let data = await response.json();
-   console.log(data);
-   let rendomImage = data.hits[0].webformatURL;
-   res.render('home.ejs', {"data": data} );
+
+//root route
+app.get('/', async (req, res) => {  //also works as app.get('/', async function(req, res) {
+    let url = "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&per_page=50&orientation=horizontal&q=solar_system"
+    let response = await fetch(url);
+    let data = await response.json();
+    //console.log(data);
+    let random0to9 = Math.floor(Math.random() * 50);
+    let randomImage = data.hits[random0to9].webformatURL;
+    res.render('home.ejs', {randomImage})
+
 });
 
-// //mercury
-// app.get('/mercury', (req, res) => {
-//    let planetInfo = solarSystem.getMercury();
-//    console.log(planetInfo);
-//    res.render('mercury.ejs', {"planetInfo": planetInfo} );
+
+app.get('/planet', (req, res) => { 
+    let planet_name =req.query.planetName;
+    let planetInfo = solarSystem[`get${planet_name}`]();
+    //console.log(planetInfo);
+    res.render('planetInfo.ejs', {planetInfo, planet_name}) 
+});
+
+app.get('/comets', (req, res) => {
+    res.render('comets.ejs')
+});
+
+app.get('/asteroids', (req, res) => {
+    res.render('asteroids.ejs')
+});
+
+app.get('/nasaPOD', async (req, res) => {
+    let url = "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&per_page=50&orientation=horizontal&q=solar_system"
+    let response = await fetch(url);
+    let data = await response.json();
+    res.render('nasaPod.ejs')
+});
+
+//mercury route
+// app.get('/mercury', (req, res) => { 
+//     let planetInfo = solarSystem.getMercury();
+//     console.log(planetInfo);
+//     res.render('mercury.ejs', {planetInfo}) //can do {"planetInfo" : planetInfo} if you want to save it as someting else
 // });
 
-app.get('/planet', (req, res) => {
-   let planet_Name = req.query.planetName;
-
-   let planetInfo = solarSystem[`get${planet_Name}`]();
-
-   console.log(planetInfo);
-   res.render('planetInfo.ejs', {planetInfo: planetInfo} );
-});
-
 app.listen(3000, () => {
-   console.log('server started');
+    console.log('server started');
 });
-
